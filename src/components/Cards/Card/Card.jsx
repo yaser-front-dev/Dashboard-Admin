876fe0e1689260a0c0efb9ba41d23450c150aeb3
yaser abbasi <yasrbasy087@gmail.com>
2024-11-22 12:47:00 +0330

    add recharts to project

--- src/components/Cards/Card/Card.jsx
+++ src/components/Cards/Card/Card.jsx
@@ -5,7 +5,7 @@ import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 export default function Card({title , price , num}) {
   return (
     <>
-    <div className="p-7 shadow-lg rounded-lg w-full">
+    <div className="p-7 shadow-md rounded-lg w-full">
         <h2 className='text-black text-lg'>{title}</h2>
         <div className="flex items-center gap-4">
             <p className='font-bold text-black text-2xl'>${price}</p>
